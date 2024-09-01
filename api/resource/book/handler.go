eebc141d6211496bcf1fbf0c10a29296b3f08ee0
JakubPluta <plutakuba@gmail.com>
2024-09-01 08:17:50 +0200

    feat: api handler added

--- api/resource/book/handler.go
+++ api/resource/book/handler.go
@@ -1,8 +1,11 @@
 package book
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
@@ -26,7 +29,20 @@ func New(db *gorm.DB) *API {
 //	@success        200 {array}     DTO
 //	@failure        500 {object}    err.Error
 //	@router         /books [get]
-func (a *API) List(w http.ResponseWriter, r *http.Request) {}
+func (a *API) List(w http.ResponseWriter, r *http.Request) {
+	books, err := a.repository.List()
+	if err != nil {
+		return
+	}
+	if len(books) == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(books.ToDto()); err != nil {
+		return
+	}
+}
 
 // Read godoc
 //
@@ -41,7 +57,26 @@ func (a *API) List(w http.ResponseWriter, r *http.Request) {}
 //	@failure        404
 //	@failure        500 {object}    err.Error
 //	@router         /books/{id} [get]
-func (a *API) Read(w http.ResponseWriter, r *http.Request) {}
+func (a *API) Read(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		return
+	}
+
+	book, err := a.repository.Read(id)
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		return
+	}
+	dto := book.ToDto()
+	if err := json.NewEncoder(w).Encode(dto); err != nil {
+		return
+	}
+}
 
 // Create godoc
 //
@@ -56,7 +91,21 @@ func (a *API) Read(w http.ResponseWriter, r *http.Request) {}
 //	@failure        422 {object}    err.Errors
 //	@failure        500 {object}    err.Error
 //	@router         /books [post]
-func (a *API) Create(w http.ResponseWriter, r *http.Request) {}
+func (a *API) Create(w http.ResponseWriter, r *http.Request) {
+	form := &Form{}
+	if err := json.NewDecoder(r.Body).Decode(form); err != nil {
+		return
+	}
+	newBook := form.ToModel()
+	newBook.ID = uuid.New()
+
+	_, err := a.repository.Create(newBook)
+	if err != nil {
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
 
 // Update godoc
 //
@@ -73,7 +122,31 @@ func (a *API) Create(w http.ResponseWriter, r *http.Request) {}
 //	@failure        422 {object}    err.Errors
 //	@failure        500 {object}    err.Error
 //	@router         /books/{id} [put]
-func (a *API) Update(w http.ResponseWriter, r *http.Request) {}
+func (a *API) Update(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		return
+	}
+
+	form := &Form{}
+	if err := json.NewDecoder(r.Body).Decode(form); err != nil {
+		return
+	}
+
+	book := form.ToModel()
+	book.ID = id
+	rows, err := a.repository.Update(book)
+	if err != nil {
+		return
+	}
+
+	if rows == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
 
 // Delete godoc
 //
@@ -88,4 +161,20 @@ func (a *API) Update(w http.ResponseWriter, r *http.Request) {}
 //	@failure        404
 //	@failure        500 {object}    err.Error
 //	@router         /books/{id} [delete]
-func (a *API) Delete(w http.ResponseWriter, r *http.Request) {}
+func (a *API) Delete(w http.ResponseWriter, r *http.Request) {
+	id, err := uuid.Parse(chi.URLParam(r, "id"))
+	if err != nil {
+		// handle later
+		return
+	}
+
+	rows, err := a.repository.Delete(id)
+	if err != nil {
+		// handle later
+		return
+	}
+	if rows == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+}
--- api/resource/book/model.go
+++ api/resource/book/model.go
@@ -7,6 +7,23 @@ import (
 	"gorm.io/gorm"
 )
 
+type DTO struct {
+	ID            string `json:"id"`
+	Title         string `json:"title"`
+	Author        string `json:"author"`
+	PublishedDate string `json:"published_date"`
+	ImageURL      string `json:"image_url"`
+	Description   string `json:"description"`
+}
+
+type Form struct {
+	Title         string `json:"title"`
+	Author        string `json:"author"`
+	PublishedDate string `json:"published_date"`
+	ImageURL      string `json:"image_url"`
+	Description   string `json:"description"`
+}
+
 type Book struct {
 	ID            uuid.UUID `gorm:"primarykey"`
 	Title         string
@@ -20,3 +37,34 @@ type Book struct {
 }
 
 type Books []*Book
+
+func (f *Form) ToModel() *Book {
+	pubDate, _ := time.Parse("2006-01-02", f.PublishedDate)
+
+	return &Book{
+		Title:         f.Title,
+		Author:        f.Author,
+		PublishedDate: pubDate,
+		ImageURL:      f.ImageURL,
+		Description:   f.Description,
+	}
+}
+
+func (b *Book) ToDto() *DTO {
+	return &DTO{
+		ID:            b.ID.String(),
+		Title:         b.Title,
+		Author:        b.Author,
+		PublishedDate: b.PublishedDate.Format("2006-01-02"),
+		ImageURL:      b.ImageURL,
+		Description:   b.Description,
+	}
+}
+
+func (bs Books) ToDto() []*DTO {
+	dtos := make([]*DTO, len(bs))
+	for i, v := range bs {
+		dtos[i] = v.ToDto()
+	}
+	return dtos
+}
